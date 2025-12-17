9d145626cbf932521c49eb5d727c22076ba8bfda
JackyKIMSungSu <sskim7415@gmail.com>
2025-12-17 18:10:30 +0900

    feat: Adjust num range on main branch

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1, 15+1):
-    if i % 5 == 0:
+for i in range(1, 18+1):
+    if i % 3 == 0:
         print('fizz')
+    elsif i % 5 == 0:
+        print('buzz')
     else:
         print(i)
